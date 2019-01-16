2386f9f7986a9aa8047989858c65c0112dda87b8
Thomas Keady <tomkeady1@gmail.com>
2019-01-16 19:17:23 +0000

    Initial commit

---
+++ 16608272 project1 final/project1 final/project1 final/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.IO;
+class textAnalyse
+{
+
+    public void readText()
+    {
+        Console.WriteLine("Enter text, end each sentence with  * (asterisk)"); // User enters text to be saved in string
+        string userEnteredText = Console.ReadLine();
+
+        File.AppendAllText("TextEntry.txt", userEnteredText); // Adds entered text to new file then reads said file
+        string readEnteredText = File.ReadAllText("TextEntry.txt");
+
+        // User confirmation
+        Console.WriteLine("Finished?\nInput 'Yes' if done or 'No' if not");
+
+        // Save entry as string and transfers to switch statement
+        string userChoice = Console.ReadLine();
+        string userChoiceLowered = userChoice.ToLower();
+
+        // Check if user is done
+        switch (userChoiceLowered)
+        {
+            // If no run readText() again
+            case "no":
+                readText();
+                break;
+
+            case "yes": // basicResponse() gives analysis on 'yes'
+                Console.WriteLine("Sentence analysis:");
+                basicResponse(readEnteredText);
+                break;
+
+            default: // Default input assumes yes
+                Console.WriteLine("Default = yes");
+                Console.WriteLine("Sentence analysis:");
+                basicResponse(readEnteredText);
+                break;
+        }
+    }
+
+    public void basicResponse(string enteredText)
+    {
+        char[] enteredTextArray = enteredText.ToCharArray(); // Text is enteredText and converts to char array for statements 
+
+        // Commands get own string from defined function
+        Console.WriteLine("Number of sentences entered: {0}", returnsentence(enteredTextArray));
+        Console.WriteLine("Number of vowels: {0}", returnVowel(enteredTextArray));
+        Console.WriteLine("Number of consonants: {0}", returnConsonant(enteredTextArray));
+        Console.WriteLine("Number of upper case letters: {0}", returnUpper(enteredTextArray));
+        Console.WriteLine("Number of lower case letters: {0}", returnLower(enteredTextArray));
+
+        Console.WriteLine("Would you like to see the frequency of an specific letter?"); // Ask user if they want the letter frequency
+        string userChoiceLetterFrequency = Console.ReadLine().ToLower();
+        switch (userChoiceLetterFrequency)
+        {
+
+            case "yes": // If yes use returnIndividualLetters()
+                returnIndividualLetters(enteredTextArray);
+                break;
+
+            case "no": // If no close program and run deleteTextFile
+                deleteTextFile();
+                break;
+
+            default: // Assume no and use deleteTextFile and close program
+                deleteTextFile();
+                break;
+        }
+    }
+
+    public void returnIndividualLetters(char[] enteredTextString)
+    { // Retrieve letter frequency
+        Console.WriteLine("Enter the letter you want the frequency of:");
+
+        try
+        { // Get selected letter
+            string userLetter = Console.ReadLine().ToLower();
+            int userLetterAmount = 0; // foreach will find all characters and single out the specific letter
+            foreach (char i in enteredTextString)
+            {
+                if (i.ToString().ToLower() == userLetter)
+                {
+                    userLetterAmount++; // Add up the total occurence of the letter
+                }
+            }
+
+
+            Console.WriteLine("Letter {0} found {1} times", userLetter.ToUpper(), userLetterAmount);
+            deleteTextFile();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected Input error {0}", e);
+            deleteTextFile();
+        }
+
+    }
+
+    public string returnsentence(char[] enteredTextString)
+    {  // Return sentence amount using asterisk*
+        int sentenceAmount = 0;
+        foreach (char i in enteredTextString)
+        { // Check every character
+            switch (i)
+            {
+                case '*':
+                    //If it is a * then increment the sentenceAmount count
+                    sentenceAmount++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (sentenceAmount == 0)
+        {
+            sentenceAmount++;
+        }
+        //Return the sentenceAmount in string form for Console to Write
+        return sentenceAmount.ToString();
+    }
+
+    //Return the amount of vowels in the text
+    public string returnVowel(char[] enteredTextString)
+    {
+        int vowelAmount = 0;
+
+        //Check every character in the string
+        foreach (char i in enteredTextString)
+        {
+            // vowelAmount incrementation method
+            if ("AEIOUaeiou".Contains(i.ToString()))
+            { // Vowel list
+                vowelAmount++;
+            }
+        }
+        return vowelAmount.ToString(); // vowelAmount to string for console
+    }
+
+    public string returnConsonant(char[] enteredTextString)
+    { // Return consonants
+        char[] consonantList = { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' }; // Consonant list
+        int consonantAmount = 0;
+        // For loop inside foreach loop to check char.
+        foreach (char i in enteredTextString)
+        { // Every found consonant identified as a match which adds to the consonant count
+            for (int j = 0; j < consonantList.Length; j++)
+            {
+                if (i == consonantList[j])
+                {
+                    consonantAmount++; // Consonant increments
+                }
+            }
+        }
+        return consonantAmount.ToString();
+    }
+
+    public string returnUpper(char[] enteredTextString)
+    { // Uppercase letter return
+        int upperAmount = 0;
+        foreach (char i in enteredTextString)
+        {
+            if (char.IsUpper(i))
+            { // IsUpper finds uppercase letters and adds to total
+                upperAmount++;
+            }
+        }
+        return upperAmount.ToString();
+    }
+
+    public string returnLower(char[] enteredTextString)
+    { // Lowercase return same as above
+        int lowerAmount = 0;
+        foreach (char i in enteredTextString)
+        {
+            if (char.IsLower(i))
+            { // Used IsLower similarly to IsUpper
+                lowerAmount++;
+            }
+        }
+        return lowerAmount.ToString();
+    }
+
+    public string returnLongWords(string enteredTextString)
+    { // For finding 'long words'
+        string[] enteredTextConvert;
+        enteredTextConvert = enteredTextString.Split(' ');
+
+        int longWordAmount = 0; // Amount set to 0
+        foreach (string i in enteredTextConvert)
+        {
+            if (i.Length > 7)
+            { // If length of word (string in array) is more than seven characters then 1 is added 
+                longWordAmount++;
+                File.AppendAllText("longWord.txt", i + Environment.NewLine);
+            }
+        }
+
+        return longWordAmount.ToString(); // For console to write
+    }
+
+    public void readFile()   // Option 2 to read text file
+    {
+        // Prompt user to enter file location
+        Console.WriteLine(" Enter file path of the .txt file\nIf you input 'default' an example file will run");
+        Console.Write("File path: ");
+        string userFilePath = Console.ReadLine();
+        switch (userFilePath)
+        {
+            case "default": // Opens the .txt from Blackboard about Yahoo
+                string defaultFilePath = "text.txt";
+                string[] defaultFileRead = File.ReadAllLines(defaultFilePath);
+                string defaultFileString = string.Join("", defaultFileRead); // Needs to be string like earlier
+                Console.WriteLine("Found {0} word(s) with a length greater than 7!", returnLongWords(defaultFileString));
+                basicResponse(defaultFileString); // basicResponse function
+                break;
+            default:
+                try
+                {
+                    string[] userFileRead = File.ReadAllLines(userFilePath); // Reads the .txt file from the user's file path
+                    string userFileString = string.Join("", userFileRead); // Again needs to be a string
+                    Console.WriteLine("Found {0} words with a length greater than 7", returnLongWords(userFileString));
+                    basicResponse(userFileString);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nRead file error - Is the file location correct, add .txt onto end of file name?\n Returning to previous input"); // An error will let the user try again
+                    readFile();
+                }
+                break;
+        }
+    }
+
+    public void deleteTextFile()
+    { // deleteTextFile function
+        File.Delete("TextEntry.txt"); // Delete temporary text file
+    }
+
+    public static void Main()
+    { // So that functions and method can be found
+        textAnalyse textAnalysis = new textAnalyse();
+
+        Console.WriteLine("Do you want to enter text or read text from a .txt file?"); //Introduction
+        Console.WriteLine("1. Enter the text with keyboard \n2. Read text from a .txt file");
+
+        try
+        { // Checking if user did 1 or 2 and the subsequent process
+            int keyboardOrFile = Convert.ToInt32(Console.ReadLine());
+            if (keyboardOrFile == 1)
+            {
+                textAnalysis.readText();
+            }
+            else if (keyboardOrFile == 2)
+            {
+                textAnalysis.readFile();
+            }
+        }
+        catch (Exception e)
+        { // Prevents crash and closes program
+            Console.WriteLine("Invalid");
+        }
+
+    }
+} // 16608272 Thomas Keady
---
+++ project2 final/project2 final/Program.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace project2b
+{
+    class Program
+    {
+        public class Game // 'Game' class
+        {
+            static Player[] players;
+            static int turn = 0;
+            static int maximumScore = 50;
+            public bool isGameOver = false;
+            public Game(int playerAmount, int nowMaximumScore)
+            {
+                if (playerAmount == 1)
+                {
+                    players = new Player[playerAmount + 1];
+                    players[0] = new Player(false); // Check if AI is needed
+                    players[1] = new Player(true);
+                }
+                else
+                {
+                    players = new Player[playerAmount]; // Add new human player
+                    for (int i = 0; i < playerAmount;
+                        i++)
+                    {
+                        players[i] = new Player(false);
+                    }
+                }
+                maximumScore = nowMaximumScore; // Set score
+            }
+            public void nextTurn()
+            {
+                Console.WriteLine("Three or more dice game"); // User intro to game
+                Console.WriteLine("Players: " + players.Length); // Player amount
+                Console.WriteLine("Goal: " + maximumScore);
+
+                players[turn].turn();
+                if (players[turn].getScore() >= maximumScore)
+                {
+                    endGame(turn); // End game conditions reached
+                }
+                else
+                {
+                    turn++; // New turn
+                    if (turn > players.Length - 1)
+                    {
+                        turn = 0;
+                    }
+                }
+            }
+            public void endGame(int winner) // For when the target score is attained
+            {
+                Console.Clear(); // Clear display
+                Console.WriteLine(players[winner].getName() + " wins"); // Winner declared
+                Console.ReadKey(true); // Game will loop otherwise
+
+                Console.WriteLine("Statistics: "); // Player statistics
+                int totalDiceRolls = 0;
+                int[] diceRollValues = { 0, 0, 0, 0, 0, 0 };
+                int totalNumber = 0;
+                for (int i = 0; i < players.Length; i++)
+                {
+                    Console.WriteLine(i);
+                    totalDiceRolls += players[i].previousRolls.Count;
+                    foreach (int roll in players[i].previousRolls)
+                    {
+                        diceRollValues[roll - 1]++;
+                        totalNumber = totalNumber + roll;
+                    }
+                    isGameOver = true;
+                }
+                int averageRoll = totalNumber / totalDiceRolls;
+                // Displays all statistics on console
+                Console.WriteLine("Total dice rolls: " + totalDiceRolls);
+                Console.WriteLine("1 was rolled " + diceRollValues[0] + " times.");
+                Console.WriteLine("2 was rolled " + diceRollValues[1] + " times.");
+                Console.WriteLine("3 was rolled " + diceRollValues[2] + " times.");
+                Console.WriteLine("4 was rolled " + diceRollValues[3] + " times.");
+                Console.WriteLine("5 was rolled " + diceRollValues[4] + " times.");
+                Console.WriteLine("6 was rolled " + diceRollValues[5] + " times.");
+                Console.WriteLine("Average roll: " + averageRoll);
+                Console.WriteLine("Total dice number: " + totalNumber);
+                Console.ReadLine();
+            }
+        }
+        public class Player // 'Player' class
+        {
+            int score = 0;
+            bool PrimitiveAI;
+            public List<int> previousRolls = new List<int>(); // List to store old turns
+            string name = "player";
+            Die[] dice = new Die[5];
+            public Player(bool isAI) // For computer controlled opponent
+
+            {
+                PrimitiveAI = isAI;
+                for (int i = 0; i < 5; i++)
+                {
+                    dice[i] = new Die();
+                }
+                if (PrimitiveAI == false)
+                {
+                    Console.WriteLine("Enter name"); // Player 2 name entry
+                    name = Console.ReadLine();
+                }
+                else
+                {
+                    name = "Dice Master"; // AI name
+                }
+            }
+            public int turn()
+            {
+                int turnScore = 0;
+                Console.WriteLine("Your turn, " + name + ".\nScore: " + score + "\n(1) - Throw and reroll three\n(2) - Throw all dice *double points*"); // Turn options
+                char option;
+                if (PrimitiveAI == true)
+                {
+                    Thread.Sleep(250); // These indicate momentary pauses
+                    option = '1';
+                }
+                else
+                {
+                    option = Console.ReadKey().KeyChar;
+                }
+                if (option == '1') // If user selects option 1 to throw repeatedly
+                {
+                    Console.WriteLine("\nPress any key to roll");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (PrimitiveAI == true)
+                        {
+                            Thread.Sleep(250);
+                        }
+                        else
+                        {
+                            Console.ReadKey(true);
+                        }
+                        dice[i].roll();
+                        Console.Write(", "); // Displays each dice roll
+                        previousRolls.Add(dice[i].getValue()); // Adds new dice score to current score
+                    }
+
+                    Console.WriteLine("\nAll dice rolled. Press any key to continue");
+                    if (PrimitiveAI == true)
+                    {
+                        Thread.Sleep(500);
+                    }
+                    else
+                    {
+                        Console.ReadKey(true);
+                    }
+                    turnScore = calculateScore();
+                    addScore(turnScore);
+                }
+                else if (option == '2') // If user selects option 2
+                {
+                    Console.WriteLine("\nPress any key to throw all dice at once");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        dice[i].roll();
+                        Console.Write(", ");
+                        previousRolls.Add(dice[i].getValue());
+                    }
+                    Console.WriteLine("\nAll dice thrown, press any key to continue");
+                    if (PrimitiveAI == true)
+                    {
+                        Thread.Sleep(500);
+                    }
+                    else
+                    {
+                        Console.ReadKey(true);
+                    }
+                    turnScore = calculateScore(true) * 2;
+                    addScore(turnScore);
+                }
+                Console.WriteLine("You scored " + turnScore, "Press any key to continue");
+                if (PrimitiveAI == true)
+                {
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Console.ReadKey(true);
+                }
+                return 0;
+            }
+            public int calculateScore(bool threwAll = false)
+            {
+                Console.WriteLine("Result:");
+                int[] results = { 0, 0, 0, 0, 0, 0 }; // Shows results
+                for (int i = 0; i < 5; i++)
+                {
+                    results[dice[i].getValue() - 1]++; // Goes through dice
+                }
+                int[] bestResult = calculateBestResult(results); // Finds maximum possible score from turn
+                Console.WriteLine(bestResult[0] + " of a kind! (" + bestResult[1] + ")");
+                if (bestResult[0] == 2 && threwAll == false) // If low score gives chance to re-roll three dice
+                {
+                    Console.WriteLine("Reroll three dice, press any key to continue");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (dice[i].getValue() != bestResult[1])
+                        {
+                            if (PrimitiveAI == true)
+                            {
+                                Thread.Sleep(250);
+                            }
+                            else
+                            {
+                                Console.ReadKey(true);
+                            }
+                            dice[i].roll();
+                            results[dice[i].getValue() - 1]++;
+                            Console.Write(", ");
+                            previousRolls.Add(dice[i].getValue());
+                        }
+                    }
+                    Console.WriteLine("\nAll dice thrown, press any key to continue");
+                    if (PrimitiveAI == true)
+                    {
+                        Thread.Sleep(500);
+                    }
+                    else
+                    {
+                        Console.ReadKey(true);
+                    }
+                }
+                bestResult = calculateBestResult(results);
+                switch (bestResult[0]) // Gives points depending on score
+                {
+                    case 3: // If 3 match
+                        return 3;
+                    case 4: // If 4 match
+                        return 6;
+                    case 5: // If 5 match
+                        return 12;
+                    default:
+                        return 0;
+                }
+            }
+            public int[] calculateBestResult(int[] results)
+            {
+                int[] bestResult = { 1, 0 }; // Value, dice number
+
+                for (int i = 0; i < 6; i++)
+                {
+                    if (results[i] >= bestResult[0])
+                    {
+                        bestResult[0] = results[i];
+                        bestResult[1] = i + 1;
+                    }
+                }
+
+                return bestResult;
+            }
+
+            public void addScore(int toAdd)
+            {
+                score += toAdd; // Score addition function
+            }
+            public int getScore()
+            {
+                return score;
+            }
+            public string getName()
+            {
+                return name;
+            }
+        }
+        public class Die // 'Die' class
+        {
+            int dieValue;
+            static Random round = new Random(); // Picks random number
+            public Die()
+            { // Needs brackets to work
+            }
+            public void roll()
+            {
+                dieValue = round.Next(1, 7);
+                Console.Write(dieValue);
+            }
+
+            public int getValue()
+            {
+                return dieValue;
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool isPlaying = true;
+            while (isPlaying == true)
+            {
+                Console.Clear(); // Clear display for intro
+                Console.WriteLine("Dice Game");
+                Console.Write("Number of players: ");
+                int playerCount = 1;
+                try
+                {
+                    playerCount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Error: " + e); // Error catch
+                }
+                Console.Write("Enter what you would like the goal to be ");
+                int maxScore = 50;
+                try
+                {
+                    maxScore = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Error: " + e);
+                }
+                Game game = new Game(playerCount, maxScore); // Resets game
+                while (game.isGameOver == false)
+                {
+                    Console.Clear();
+                    game.nextTurn(); // New turn
+                }
+                while (1 == 1)
+                {
+                    Console.WriteLine("\nWould you like to play again? y/n Play again? Please only enter 'y' or 'n':");
+                    char option = Console.ReadKey().KeyChar;
+                    if (option == 'n')
+                    {
+                        isPlaying = false;
+                        break;
+                    }
+                    else if (option == 'y')
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+    // 16608272 Thomas Keady
+}
